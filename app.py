e29f8e05afc0681745b8302dfea57d068e860fe8
sakigate <saki_ato_saki_1112@yahoo.co.jp>
2025-08-24 18:38:02 +0900

    返信ができるようになった

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request, flash, url_for
-from flask_login import LoginManager, current_user, login_manager,login_user, logout_user, login_required
+from flask_login import LoginManager, current_user, login_manager, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import User, Message
 
@@ -8,17 +8,20 @@ app.secret_key = "secret"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 # Flask-Loginがユーザー情報を取得するためのメソッド
 @login_manager.user_loader
 def load_user(user_id):
     return User.get_by_id(user_id)
+
 
 # ログインしていないとアクセスできないページにアクセスがあった場合の処理
 @login_manager.unauthorized_handler
 def unauthorized_handler():
     return redirect("/login")
 
-@app.route('/register',methods=["GET", "POST"])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         # データの検証 未入力の処理
@@ -38,11 +41,12 @@ def register():
         User.create(
             name=request.form["name"],
             email=request.form["email"],
-            password=generate_password_hash(request.form["password"])
+            password=generate_password_hash(request.form["password"]),
         )
-    return render_template('register.html')
+    return render_template("register.html")
 
-@app.route('/login', methods=["GET", "POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         # 未入力の時
@@ -60,18 +64,20 @@ def login():
         # ログインできなかった時
         flash("ログインできませんでした")
 
-    return render_template('login.html')
+    return render_template("login.html")
+
 
 # ログアウト処理
-@app.route('/logout')
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
     flash("ログアウトしました")
     return redirect(url_for("index"))
 
+
 # ユーザー削除
-@app.route('/unregister')
+@app.route("/unregister")
 @login_required
 def unregister():
     current_user.delete_instance()
@@ -79,15 +85,14 @@ def unregister():
     return redirect(url_for("index"))
 
 
-@app.route('/', methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         # メッセージテーブルにクリエイト(追加)
         Message.create(user=current_user, content=request.form["content"])
-    messages =(
-        Message.select()
-        .where(Message.reply_to.is_null(True))
-        #descは降順の意味
+    messages = (
+        Message.select().where(Message.reply_to.is_null(True))
+        # descは降順の意味
         .order_by(Message.pub_date.desc(), Message.id.desc())
     )
     return render_template("index.html", messages=messages)
@@ -103,10 +108,21 @@ def delete(message_id):
         flash("無効な操作です")
     return redirect(url_for("index"))
 
+
 # 返信表示
 @app.route("/messages/<message_id>/")
 def show(message_id):
-    return render_template("show.html")
+    messages = (
+        Message.select()
+        .where((Message.id == message_id) | (Message.reply_to == message_id))
+        .order_by(Message.pub_date.desc())
+    )
+    return render_template("show.html", messages=messages, message_id=message_id)
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+@app.route("/messages/<message_id>/", methods=["POST"])
+@login_required
+def reply(message_id):
+    return redirect(url_for("show", message_id=message_id))
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
